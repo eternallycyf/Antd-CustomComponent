c703d3587fb2ea8a31d262498876390464e2ca87
eternallycyf <a969475322@gmail.com>
2022-12-28 17:18:55 +0800

    fix: ts-node script error

--- scripts/generateBizConfig.ts
+++ scripts/generateBizConfig.ts
@@ -1,12 +1,7 @@
-import fs from "fs";
-import path from "path";
-import chalk from "chalk";
-import ejs from "ejs";
-import url from 'url';
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const ejs = require('ejs');
 type IBusinessList = { name: string; path: string }[];
 function getAllBiz(source: string): IBusinessList {
   if (!fs.existsSync(source)) {
@@ -24,22 +19,22 @@ function getAllBiz(source: string): IBusinessList {
   });
   return bizList;
 }
-const targetFile: string = path.resolve(__dirname, "../src/routes/routes.ts");
-const bizPath: string = path.resolve(__dirname, "../src/pages");
+const targetFile: string = path.resolve(__dirname, '../src/routes/routes.ts');
+const bizPath: string = path.resolve(__dirname, '../src/pages');
 const templatePath: string = path.resolve(
   __dirname,
-  "./biz-stage-config.ts.ejs",
+  './biz-stage-config.ts.ejs',
 );
 
 console.log(chalk.green(`配置插件...`));
 
-const template = fs.readFileSync(templatePath, "utf8");
-const bizList = getAllBiz(bizPath);
+const template: Buffer = fs.readFileSync(templatePath, 'utf8');
+const bizList: IBusinessList = getAllBiz(bizPath);
 const result = ejs.render(template, { plugins: bizList });
 
-fs.writeFile(targetFile, result, (err) => {
+fs.writeFile(targetFile, result, (err: NodeJS.ErrnoException) => {
   if (err) {
-    console.error("write file error", err);
+    console.error('write file error', err);
   } else {
     console.log(chalk.green(`配置插件完成: ${targetFile}\n`));
   }
