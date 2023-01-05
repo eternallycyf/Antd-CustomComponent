50918a6e4d4f341e13754fa571d23b15a644f2d7
eternallycyf <a969475322@gmail.com>
2023-01-05 22:54:36 +0800

    chore: update react-dnd react-dnd-html5-backend react-resizable

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -12,7 +12,4 @@ declare module 'braft-utils';
 declare module 'braft-extensions/dist/table';
 declare module 'braft-extensions/dist/max-length';
 declare module 'braft-extensions/dist/color-picker';
-declare module 'react-resizable';
-declare module 'react-dnd-html5-backend';
-declare module 'react-dnd';
 declare module 'mockjs';
