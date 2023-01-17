4bda44abb65823722c2e5514a3a062d16ea301e5
eternallycyf <a969475322@gmail.com>
2023-01-17 11:10:51 +0800

    fix: update local config

--- src/config/projectConfig.tsx
+++ src/config/projectConfig.tsx
@@ -4,9 +4,11 @@ const apiPrefix: string = '/cms_service';
 const tokenKey: string = 'token';
 
 // 部署在根路径下 请设置为 '/' 其他路径下为 '/xxx'
-export const homePage = process.env.NODE_ENV ? '/Antd-CustomComponent' : '/';
+export const homePage =
+  process.env.NODE_ENV === 'production' ? '/Antd-CustomComponent' : '/';
 // 部署在根路径下 数据返回 设置为 '' 其他路径下为 '/xxx'
-export const mockBaseUrl = process.env.NODE_ENV ? '/Antd-CustomComponent' : '';
+export const mockBaseUrl =
+  process.env.NODE_ENV === 'production' ? '/Antd-CustomComponent' : '';
 
 export default {
   apiPrefixMock,
