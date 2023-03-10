8807966a3a78b425e91e936b6425f55419063733
eternallycyf <a969475322@gmail.com>
2023-03-11 01:21:49 +0800

    chore: add tailwindcss and add snowjs

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './src/core/**/*.tsx',
+    // './src/pages/**/*.tsx',
+    // './src/core/**/*.tsx',
+    // './src/components/**/*.tsx',
+    // './src/layouts/**/*.tsx',
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  important: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
