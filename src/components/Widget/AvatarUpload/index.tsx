5eed8891fd93f50545cd0b17218284617c6e416c
eternallycyf <969475322@qq.com>
2023-04-06 20:01:43 +0800

    feat: add AvatarUpload widget

---
+++ src/components/Widget/AvatarUpload/index.tsx
@@ -0,0 +1,78 @@
+import React, { useImperativeHandle, useState } from 'react';
+import styles from './index.less';
+import { PlusOutlined, RetweetOutlined } from '@ant-design/icons';
+import { Form, Upload } from 'antd';
+import type { UploadProps } from 'antd/es/upload';
+import ImgCrop from 'antd-img-crop';
+
+const normFile = (e: any) => {
+  if (Array.isArray(e)) return e;
+  return e && e.fileList;
+};
+
+type IHandle = {
+  fileList: UploadProps['fileList'];
+  setFileList: (fileList: UploadProps['fileList']) => void;
+};
+
+interface IProps {
+  label?: string;
+  name?: string;
+  AvatarUploadRef?: any;
+}
+
+const App: React.ForwardRefRenderFunction<IHandle, IProps> = (props) => {
+  const { label = '上传头像', name = 'avatar', AvatarUploadRef } = props;
+  const [fileList, setFileList] = useState<UploadProps['fileList']>([]);
+  const UploadRef = React.useRef<any>(null);
+
+  const handleReplaceFile = async () => UploadRef.current.upload && UploadRef.current.upload.uploader.onClick();
+  const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  useImperativeHandle(AvatarUploadRef, () => ({
+    fileList,
+    setFileList,
+  }));
+
+  return (
+    <Form.Item
+      name={name}
+      label={label}
+      valuePropName="fileList"
+      getValueFromEvent={normFile}
+      rules={[
+        {
+          required: true,
+          validator: () => {
+            if (fileList && fileList?.length !== 0) return Promise.resolve('');
+            return Promise.reject(new Error('请上传头像'));
+          },
+        },
+      ]}
+    >
+      <ImgCrop showGrid showReset quality={1} rotationSlider modalTitle="编辑头像">
+        <Upload
+          ref={UploadRef}
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          listType="picture-card"
+          onPreview={handleReplaceFile}
+          onChange={handleChange}
+          multiple={false}
+          maxCount={1}
+          showUploadList={{
+            previewIcon: <RetweetOutlined className={styles.icon} />,
+          }}
+        >
+          {fileList?.length === 0 && (
+            <div>
+              <PlusOutlined />
+              <div style={{ marginTop: 8 }}>上传照片</div>
+            </div>
+          )}
+        </Upload>
+      </ImgCrop>
+    </Form.Item>
+  );
+};
+
+export default React.forwardRef(App);
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,3 +1,4 @@
 export { default as Select } from './Select';
 export { default as Trend } from './Trend';
 export { default as XScroll } from './XScroll';
+export { default as AvatarUpload } from './AvatarUpload';
