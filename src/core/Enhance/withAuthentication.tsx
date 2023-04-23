bf2ee64bf2d9579695ffa83fa05c850b4f7d859f
eternallycyf <969475322@qq.com>
2023-04-23 17:31:05 +0800

    feat✨:  add withAuthentication HOC

---
+++ src/core/Enhance/withAuthentication.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface Props {
+  [key: string]: any;
+}
+
+interface ComponentWithAuthProps extends Props {
+  isAuth: boolean;
+}
+
+export default function withAuthentication<P extends Props>(
+  Component: React.ComponentType<P>,
+): React.FC<ComponentWithAuthProps & P> {
+  return function AuthenticatedComponent(props: ComponentWithAuthProps & P) {
+    const { isAuth, ...rest } = props;
+
+    if (isAuth) {
+      return <Component {...(rest as P)} />;
+    } else {
+      return <div className="error">no Auth</div>;
+    }
+  };
+}
