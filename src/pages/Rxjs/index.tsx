ef99b8873fd4f1a3e9bb46151870e05b26a969c2
eternallycyf <969475322@qq.com>
2023-05-25 09:07:41 +0800

    feat✨: add MenuItem demo

--- src/pages/Rxjs/index.tsx
+++ src/pages/Rxjs/index.tsx
@@ -6,28 +6,28 @@ import { FormEvent } from 'react';
 
 const IndexPage = () => {
   const observable = new Rx.Observable((subscriber) => {
-    subscriber.next(1);
-    subscriber.next(2);
-    subscriber.next(3);
-    setTimeout(() => {
-      subscriber.next(4);
-      subscriber.complete();
-    }, 1000);
+    try {
+      const intervalId = setInterval(() => {
+        subscriber.next('hello world');
+        subscriber.complete();
+      }, 1000);
+      return function unsubscribe() {
+        clearInterval(intervalId);
+      };
+    } catch (error) {
+      subscriber.error(error);
+    }
   });
 
-  console.log('just before subscribe');
-  observable.subscribe({
-    next(x) {
-      console.log('got value ' + x);
-    },
-    error(err) {
-      console.error('something wrong occurred: ' + err);
-    },
-    complete() {
-      console.log('done');
-    },
+  const observable2 = Rx.from([1, 2, 3, 4, 5]);
+
+  //  observable.subscribe((value) => console.log(value));
+  const subscription = observable2.subscribe({
+    next: (value) => console.log(value),
+    complete: () => console.log('complete'),
+    error: (error) => console.log(error),
   });
-  console.log('just after subscribe');
+  subscription.unsubscribe();
 
   return <Page></Page>;
 };
