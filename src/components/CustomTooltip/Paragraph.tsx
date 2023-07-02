3bb6063c298d8c74f492f6082f68e4be582c957e
eternallycyf <969475322@qq.com>
2023-07-02 21:55:46 +0800

    feat: Paragraph add default config

--- src/components/CustomTooltip/Paragraph.tsx
+++ src/components/CustomTooltip/Paragraph.tsx
@@ -17,20 +17,21 @@ export interface IParagraph {
   rows?: number;
   copyable?: boolean;
   className?: string;
+  isDetail?: boolean;
   style?: React.CSSProperties;
   tooltipProps?: TooltipProps;
   ellipsisProps?: ParagraphProps['ellipsis'];
 }
 
 const CustomTooltipParagraph: FC<IParagraph> = (props) => {
-  const { text = '', rows = 2, tooltipProps = {}, style = {}, copyable = false, ellipsisProps, className = '', ...restProps } = props;
+  const { text = '', rows = 2, tooltipProps = {}, style = {}, copyable = false, isDetail, ellipsisProps, className = '', ...restProps } = props;
   if (_.isNil(text) || (typeof text === 'string' && text.length === 0)) return <span style={{ color: '#8E96A4' }}>--</span>;
 
   const restEllipsisProps = typeof ellipsisProps === 'object' ? ellipsisProps : {};
   return (
     <Paragraph
       style={{ marginBottom: 0, ...style }}
-      className={`${styles.desc} ${className} ${rows === 1 ? styles['single-line'] : ''}`}
+      className={`${isDetail && styles.desc} ${className} ${rows === 1 ? styles['single-line'] : ''}`}
       copyable={copyable}
       ellipsis={{
         rows,
