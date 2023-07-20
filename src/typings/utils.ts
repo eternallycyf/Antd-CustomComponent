5237e9b7a440e6cb2f51749eb1e0b7a63b68ec68
eternallycyf <969475322@qq.com>
2023-07-20 15:26:46 +0800

    type: add ValueOf

--- src/typings/utils.ts
+++ src/typings/utils.ts
@@ -1,5 +1,7 @@
 type Confine<A, B extends { [K in keyof B]: K extends keyof A ? A[K] : never }> = B;
 
+export type ValueOf<T> = T[keyof T];
+
 export type Merge<T, K> = Omit<T, keyof K> & K;
 
 export type DeepPartial<T> = T extends object
