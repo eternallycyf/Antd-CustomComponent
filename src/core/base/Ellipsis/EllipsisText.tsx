6e9dbeb427907a7e1127caca626503af9b458ac4
eternallycyf <969475322@qq.com>
2024-01-19 19:43:11 +0800

    🐛 fix: ellipsis type == length className is not working

--- src/core/base/Ellipsis/EllipsisText.tsx
+++ src/core/base/Ellipsis/EllipsisText.tsx
@@ -43,7 +43,11 @@ const EllipsisText: React.FC<EllipsisTextProps> = (props) => {
   }
   const textLength = fullWidthRecognition ? getStrFullLength(text) : text.length;
   if (textLength <= length || length < 0) {
-    return <span {...other}>{text}</span>;
+    return (
+      <span className={className} {...other}>
+        {text}
+      </span>
+    );
   }
   const tail = '...';
   let displayText;
@@ -56,7 +60,7 @@ const EllipsisText: React.FC<EllipsisTextProps> = (props) => {
   if (tooltip) {
     return (
       <Tooltip {...tooltipProps} overlayClassName={`${prefix}-tooltip`} overlayStyle={{ maxWidth: 700, ...tooltipProps?.overlayStyle }} title={text}>
-        <span>
+        <span className={className}>
           {displayText}
           {tail}
         </span>
